61778ede37c59288f0299b1ecbfe4a9332b3cf42
nichro02 <nichro02@gmail.com>
2020-10-31 09:06:25 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
+//set up ejs and ejs layouts
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+//set up middleware for controllers
+app.use('/auth', require('./controllers/auth.js'))
+
+
+app.get('/', (req, res)=> {
+    res.send('HOME ROUTE')
+})
+
+app.listen(8000, ()=>{
+    console.log('listening on 8000')
+})
---
+++ readme.md
@@ -0,0 +1,6 @@
+# EXPRESS AUTH BOILERPLATE
+* Set up Express app and entry point JS
+* Initialize git
+* Install and require ejs and express-ejs-layouts
+* Touch a .gitignore file (echo "node_modules" >> .gitignore)
+* Create views and controllers folders
